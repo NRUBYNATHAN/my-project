1f6a08e31f15fb9ac92d265c77af83cd859798b6
RUBYNATHAN N <rubynathan999@gmail.com>
2024-08-11 18:16:28 +0530

    headphone-banner-ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/hero'
+
+function App() {
+  return (
+    <main className='overflow-x-hidden'>
+      <Hero />
+    </main>
+  )
+}
+
+export default App
